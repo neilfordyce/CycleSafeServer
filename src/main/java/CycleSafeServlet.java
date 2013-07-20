b42a122050220f88faffc2401247b3c3374160e4
Calum Murray <Cal_92@hotmail.co.uk>
2013-07-20 14:56:48 +0100

    Basic Servlet Added

---
+++ src/main/java/CycleSafeServlet.java
@@ -0,0 +1,49 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Main Servlet to receive GPS data from Bikes and Lorries
+ * Performs proximity logic and notifies Lorry drivers.
+ * @author Calum
+ */
+@WebServlet(urlPatterns = {"/*"})
+public class CycleSafeServlet extends HttpServlet 
+{
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        PrintWriter writer = response.getWriter();
+        writer.println("Deployment Works");
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        
+    }
+
+}
